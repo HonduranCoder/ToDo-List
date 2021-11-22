c6e6aa198ecdd0091d214614e2362bab6826ab45
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-11-22 15:26:08 -0600

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -2,52 +2,69 @@ import { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../home/Home';
+import Form from './Form.js';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect
+  Redirect,
 } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [],
+    todoTitle: '',
+  };
+
+  inputState = (e) => {
+    //take the value of the input and put it into state
+    this.setState({ todoTitle: e.target.value });
+    console.log(this.todoTitle);
+    console.log('input state input');
+  };
+
+  addButton = (e) => {
+    e.preventDefault();
+    //input added to page
+    //set state, set state of todos, create new array, everything inside of our array & spreading content inside the array to a different array
+    this.setState({ todos: [...this.state.todos, this.state.todoTitle] });
+    console.log(this.state.todos);
+  };
 
   render() {
+    console.log(this.state.todoTitle);
     return (
       <div className="App">
         <Router>
-          <Header/>
+          <Header />
+          <Form addButton={this.addButton} inputState={this.inputState} />
+          {this.state.todos.map((item, idx) => (
+            <p key={idx}>{item}</p>
+          ))}
           <main>
-
             <Switch>
-              <Route path="/" exact={true}
-                render={routerProps => (
-                  <Home {...routerProps}/>
-                )}
+              <Route
+                path="/"
+                exact={true}
+                render={(routerProps) => <Home {...routerProps} />}
               />
 
-              <Route path="/resources" exact={true}
-                render={routerProps => (
-                  <div>Implement a page of resources</div>
-                )}
-              />
-
-              <Route path="/resources/:id"
-                render={routerProps => (
-                  <div>Implement a page for id {routerProps.match.params.id}</div>
-                )}
+              <Route
+                path="/todos"
+                exact={true}
+                render={(routerProps) => <div>Todos</div>}
               />
 
               <Redirect to="/" />
-
             </Switch>
           </main>
-          <Footer/>
+          <Footer />
         </Router>
       </div>
     );
   }
-
 }
 
 export default App;
+
