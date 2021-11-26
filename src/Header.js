8e6a4f0a37e8e855795411f47f989345bd6b7841
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-11-26 16:02:41 -0600

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+
+  render() { 
+    return (
+      <header className="Header">
+
+        <h1>ToDo</h1>
+        
+      </header>
+    );
+  }
+
+}
+ 
+export default Header;
